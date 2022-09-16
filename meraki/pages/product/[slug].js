d1767c85ddd22304bf26a3729c1904846f6f3dd4
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-09-16 18:42:03 +0530

    [FEAT]: created product page

---
+++ meraki/pages/product/[slug].js
@@ -0,0 +1,64 @@
+import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/router";
+import React from "react";
+import { Layout } from "../../components";
+import { data } from "../../utils";
+
+export default function ProductScreen() {
+  const { query } = useRouter();
+  const { slug } = query;
+  const product = data.products.find((x) => x.slug === slug);
+  if (!product) {
+    return <div>Produt Not Found</div>;
+  }
+  return (
+    <Layout title={product.name}>
+      <div className="py-2">
+        <Link href="/">back to products</Link>
+      </div>
+      <div className="grid md:grid-cols-4 md:gap-3">
+        <div className="md:col-span-2">
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            layout="responsive"
+          />
+        </div>
+        <div className="md:col-span-2">
+          <div className="mb-5">
+            <ul>
+              <li>
+                <h1 className="text-lg">{product.name}</h1>
+              </li>
+              <li>Category: {product.category}</li>
+              <li>Brand: {product.brand}</li>
+              <li>
+                {product.rating} of {product.numReviews} reviews
+              </li>
+              <li>Description: {product.description}</li>
+            </ul>
+          </div>
+          <div>
+            <div className="card p-5 w-96 bg-cyan-50">
+              <div className="mb-1 flex justify-between">
+                <div>Price: Rs.{product.price}</div>
+              </div>
+              <div className="mb-3 flex justify-between">
+                <div>
+                  Status:{" "}
+                  {product.countInStock > 0 ? "In stock" : "Unavailable"}
+                </div>
+              </div>
+              <button className="primary-button w-full" type="button">
+                Add to cart
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+}
