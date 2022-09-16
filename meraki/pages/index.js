34002478e2e1256817fb316b47565b38ad8d68c5
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-09-16 17:19:35 +0530

    [FEAT]: create next app and cleanup

---
+++ meraki/pages/index.js
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Meraki</title>
+        <meta name="description" content="An E-commerce application" />
+      </Head>
+
+      <h1>MERAKI</h1>
+    </div>
+  );
+}
