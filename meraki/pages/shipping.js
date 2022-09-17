1d8a7e9a33484df50128ac12476142994df05d75
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-09-17 11:09:38 +0530

    [FEAT]: created shipping page

---
+++ meraki/pages/shipping.js
@@ -0,0 +1,143 @@
+/* eslint-disable jsx-a11y/no-autofocus */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import Cookies from "js-cookie";
+import { useRouter } from "next/router";
+import React, { useContext, useEffect } from "react";
+import { useForm } from "react-hook-form";
+import { CheckoutWizard, Layout } from "../components";
+import { Store } from "../utils";
+
+export default function ShippingScreen() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    setValue,
+  } = useForm();
+
+  const { state, dispatch } = useContext(Store);
+  const { cart } = state;
+  const { shippingAddress } = cart;
+  const router = useRouter();
+
+  useEffect(() => {
+    setValue("fullName", shippingAddress.fullName);
+    setValue("address", shippingAddress.address);
+    setValue("city", shippingAddress.city);
+    setValue("postalCode", shippingAddress.postalCode);
+    setValue("country", shippingAddress.country);
+  }, [setValue, shippingAddress]);
+
+  const submitHandler = ({ fullName, address, city, postalCode, country }) => {
+    dispatch({
+      type: "SAVE_SHIPPING_ADDRESS",
+      payload: { fullName, address, city, postalCode, country },
+    });
+    Cookies.set(
+      "cart",
+      JSON.stringify({
+        ...cart,
+        shippingAddress: {
+          fullName,
+          address,
+          city,
+          postalCode,
+          country,
+        },
+      }),
+    );
+
+    router.push("/payment");
+  };
+
+  return (
+    <Layout>
+      <CheckoutWizard activeStep={0} />
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className="mb-4 text-xl">Shipping Address</h1>
+        <div className="mb-4">
+          <label htmlFor="fullName">Full Name</label>
+          <input
+            className="w-full"
+            id="fullName"
+            autoFocus
+            {...register("fullName", {
+              required: "Please enter full name",
+            })}
+          />
+          {errors.fullName && (
+            <div className="text-red-500">{errors.fullName.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="address">Address</label>
+          <input
+            className="w-full"
+            id="address"
+            autoFocus
+            {...register("address", {
+              required: "Please enter address",
+              minLength: {
+                value: 5,
+                message: "Address must be atleast 5 characters",
+              },
+            })}
+          />
+          {errors.address && (
+            <div className="text-red-500">{errors.address.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="city">City</label>
+          <input
+            className="w-full"
+            id="city"
+            autoFocus
+            {...register("city", {
+              required: "Please enter city",
+            })}
+          />
+          {errors.city && (
+            <div className="text-red-500">{errors.city.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="postalCode">Postal Code</label>
+          <input
+            className="w-full"
+            id="postalCode"
+            autoFocus
+            {...register("postalCode", {
+              required: "Please enter postal code",
+            })}
+          />
+          {errors.postalCode && (
+            <div className="text-red-500">{errors.postalCode.message}</div>
+          )}
+        </div>
+        <div className="mb-7">
+          <label htmlFor="country">Country</label>
+          <input
+            className="w-full"
+            id="country"
+            autoFocus
+            {...register("country", {
+              required: "Please enter country",
+            })}
+          />
+          {errors.country && (
+            <div className="text-red-500">{errors.country.message}</div>
+          )}
+        </div>
+        <div className="mb-4 flex justify-center">
+          <button className="primary-button w-2/5">Next</button>
+        </div>
+      </form>
+    </Layout>
+  );
+}
+
+ShippingScreen.auth = true;
