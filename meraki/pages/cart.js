e68243d3982c0685f0fb3bcebf2602e940c66148
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-09-21 05:15:14 +0530

    [FEAT]: loaded products from backend

--- meraki/pages/cart.js
+++ meraki/pages/cart.js
@@ -2,6 +2,8 @@
 import Image from "next/image";
 import Link from "next/link";
 import React, { useContext } from "react";
+import { toast } from "react-toastify";
+import axios from "axios";
 import { useRouter } from "next/router";
 import dynamic from "next/dynamic";
 import Layout from "../components/Layout";
@@ -18,9 +20,14 @@ function CartScreen() {
     dispatch({ type: "CART_REMOVE_ITEM", payload: item });
   };
 
-  const updateCartHandler = (item, qty) => {
+  const updateCartHandler = async (item, qty) => {
     const quantity = Number(qty);
+    const { data } = await axios.get(`/api/products/${item._id}`);
+    if (data.countInStock < quantity) {
+      return toast.error("Sorry, Product is out of stock.");
+    }
     dispatch({ type: "CART_ADD_ITEM", payload: { ...item, quantity } });
+    toast.success("Product quantity updated.");
   };
 
   return (
--- meraki/pages/product/[slug].js
+++ meraki/pages/product/[slug].js
@@ -1,26 +1,33 @@
+import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import React, { useContext } from "react";
+import { toast } from "react-toastify";
 import { Layout } from "../../components";
-import { data, Store } from "../../utils";
+import { Product } from "../../models";
+import { db, Store } from "../../utils";
 
-export default function ProductScreen() {
+export default function ProductScreen(props) {
+  const { product } = props;
   const { state, dispatch } = useContext(Store);
   const router = useRouter();
-  const { query } = useRouter();
-  const { slug } = query;
-  const product = data.products.find((x) => x.slug === slug);
+
   if (!product) {
-    return <div>Produt Not Found</div>;
+    return (
+      <Layout>
+        <div>Product Not Found</div>
+      </Layout>
+    );
   }
 
-  const addToCartHandler = () => {
+  const addToCartHandler = async () => {
     const existItem = state.cart.cartItems.find((x) => x.slug === product.slug);
     const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
 
-    if (product.countInStock < quantity) {
-      alert("Sorry. Product is out of stock");
+    if (data.countInStock < quantity) {
+      toast.error("Sorry, Product is out of stock.");
       return;
     }
 
@@ -80,4 +87,18 @@ export default function ProductScreen() {
       </div>
     </Layout>
   );
+}
+
+export async function getServerSideProps(context) {
+  const { params } = context;
+  const { slug } = params;
+
+  await db.connect();
+  const product = await Product.findOne({ slug }).lean();
+  await db.disconnect();
+  return {
+    props: {
+      product: product ? db.convertDocToObj(product) : null,
+    },
+  };
 }
