658b6fe7f97dee6124a955b61ad4bb662483da80
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-09-27 14:45:50 +0530

    [FEAT]: created admin dashboard

--- meraki/components/Layout.js
+++ meraki/components/Layout.js
@@ -98,6 +98,16 @@ export default function Layout({ children }) {
                         Order History
                       </DropdownLink>
                     </Menu.Item>
+                    {session.user.isAdmin && (
+                      <Menu.Item>
+                        <DropdownLink
+                          className="dropdown-link"
+                          href="/admin/dashboard"
+                        >
+                          Admin Dashboard
+                        </DropdownLink>
+                      </Menu.Item>
+                    )}
                     <Menu.Item>
                       <a
                         className="dropdown-link"
---
+++ meraki/pages/admin/dashboard.js
@@ -0,0 +1,148 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+/* eslint-disable no-unused-expressions */
+import React, { useEffect, useReducer } from "react";
+import axios from "axios";
+import Link from "next/link";
+import { Bar } from "react-chartjs-2";
+
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+} from "chart.js";
+import { getError } from "../../utils";
+import { Layout } from "../../components";
+
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend,
+);
+
+export const options = {
+  responsive: true,
+  plugins: {
+    legend: {
+      position: "top",
+    },
+  },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, summary: action.payload, error: "" };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      state;
+  }
+}
+function AdminDashboardScreen() {
+  const [{ loading, error, summary }, dispatch] = useReducer(reducer, {
+    loading: true,
+    summary: { salesData: [] },
+    error: "",
+  });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: "FETCH_REQUEST" });
+        const { data } = await axios.get(`/api/admin/summary`);
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  const data = {
+    labels: summary.salesData.map((x) => x._id),
+    datasets: [
+      {
+        label: "Sales",
+        backgroundColor: "#0e7490",
+        data: summary.salesData.map((x) => x.totalSales),
+      },
+    ],
+  };
+  return (
+    <Layout title="Admin Dashboard">
+      <div className="grid  md:grid-cols-4 md:gap-16">
+        <div className="card h-fit p-5 bg-cyan-50 text-slate-900">
+          <ul className="flex flex-col gap-6">
+            <li>
+              <Link href="/admin/dashboard">
+                <a className="font-bold">Dashboard</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/admin/orders">Orders</Link>
+            </li>
+            <li>
+              <Link href="/admin/products">Products</Link>
+            </li>
+            <li>
+              <Link href="/admin/users">Users</Link>
+            </li>
+          </ul>
+        </div>
+        <div className="md:col-span-3">
+          <h1 className="text-xl">Admin Dashboard</h1>
+          {loading ? (
+            <div className="mt-4">Loading...</div>
+          ) : error ? (
+            <div className="alert-error mt-4">{error}</div>
+          ) : (
+            <div>
+              <div className="grid grid-cols-1 md:grid-cols-4">
+                <div className="card m-5 p-5 bg-cyan-50 text-slate-900">
+                  <p className="text-3xl">Rs.{summary.ordersPrice} </p>
+                  <p>Sales</p>
+                  <Link href="/admin/orders">View sales</Link>
+                </div>
+                <div className="card m-5 p-5 bg-cyan-50 text-slate-900">
+                  <p className="text-3xl">{summary.ordersCount} </p>
+                  <p>Orders</p>
+                  <Link href="/admin/orders">View orders</Link>
+                </div>
+                <div className="card m-5 p-5 bg-cyan-50 text-slate-900">
+                  <p className="text-3xl">{summary.productsCount} </p>
+                  <p>Products</p>
+                  <Link href="/admin/products">View products</Link>
+                </div>
+                <div className="card m-5 p-5 bg-cyan-50 text-slate-900">
+                  <p className="text-3xl">{summary.usersCount} </p>
+                  <p>Users</p>
+                  <Link href="/admin/users">View users</Link>
+                </div>
+              </div>
+              <h2 className="text-xl">Sales Report</h2>
+              <Bar
+                options={{
+                  legend: { display: true, position: "right" },
+                }}
+                data={data}
+              />
+            </div>
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+AdminDashboardScreen.auth = { adminOnly: true };
+export default AdminDashboardScreen;
