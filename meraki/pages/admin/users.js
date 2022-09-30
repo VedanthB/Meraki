6482734c74cd75a261b67b34f9b3615f1a9de1bb
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2022-09-30 18:41:52 +0530

    [FEAT]: list users and delete users functionality

---
+++ meraki/pages/admin/users.js
@@ -0,0 +1,142 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import axios from "axios";
+import Link from "next/link";
+import React, { useEffect, useReducer } from "react";
+import { toast } from "react-toastify";
+import { Layout } from "../../components";
+import { getError } from "../../utils";
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "FETCH_REQUEST":
+      return { ...state, loading: true, error: "" };
+    case "FETCH_SUCCESS":
+      return { ...state, loading: false, users: action.payload, error: "" };
+    case "FETCH_FAIL":
+      return { ...state, loading: false, error: action.payload };
+
+    case "DELETE_REQUEST":
+      return { ...state, loadingDelete: true };
+    case "DELETE_SUCCESS":
+      return { ...state, loadingDelete: false, successDelete: true };
+    case "DELETE_FAIL":
+      return { ...state, loadingDelete: false };
+    case "DELETE_RESET":
+      return { ...state, loadingDelete: false, successDelete: false };
+    default:
+      return state;
+  }
+}
+
+function AdminUsersScreen() {
+  const [{ loading, error, users, successDelete, loadingDelete }, dispatch] =
+    useReducer(reducer, {
+      loading: true,
+      users: [],
+      error: "",
+    });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        dispatch({ type: "FETCH_REQUEST" });
+        const { data } = await axios.get(`/api/admin/users`);
+        dispatch({ type: "FETCH_SUCCESS", payload: data });
+      } catch (err) {
+        dispatch({ type: "FETCH_FAIL", payload: getError(err) });
+      }
+    };
+    if (successDelete) {
+      dispatch({ type: "DELETE_RESET" });
+    } else {
+      fetchData();
+    }
+  }, [successDelete]);
+
+  const deleteHandler = async (userId) => {
+    if (!window.confirm("Are you sure?")) {
+      return;
+    }
+    try {
+      dispatch({ type: "DELETE_REQUEST" });
+      await axios.delete(`/api/admin/users/${userId}`);
+      dispatch({ type: "DELETE_SUCCESS" });
+      toast.success("User deleted successfully");
+    } catch (err) {
+      dispatch({ type: "DELETE_FAIL" });
+      toast.error(getError(err));
+    }
+  };
+
+  return (
+    <Layout title="Users">
+      <div className="grid md:grid-cols-4 md:gap-16">
+        <div className="card h-fit p-5 bg-cyan-50 text-slate-900">
+          <ul className="flex flex-col gap-6">
+            <li>
+              <Link href="/admin/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link href="/admin/orders">Orders</Link>
+            </li>
+            <li>
+              <Link href="/admin/products">Products</Link>
+            </li>
+            <li>
+              <Link href="/admin/users">
+                <a className="font-bold">users</a>
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="overflow-x-auto md:col-span-3">
+          <h1 className="mb-4 text-xl">Users</h1>
+          {loadingDelete && <div>Deleting...</div>}
+          {loading ? (
+            <div>Loading...</div>
+          ) : error ? (
+            <div className="alert-error">{error}</div>
+          ) : (
+            <div className="overflow-x-auto">
+              <table className="min-w-full">
+                <thead className="border-b">
+                  <tr>
+                    <th className="px-5 text-center">ID</th>
+                    <th className="p-5 text-center">NAME</th>
+                    <th className="p-5 text-center">EMAIL</th>
+                    <th className="p-5 text-center">ADMIN</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {users.map((user) => (
+                    <tr key={user._id} className="border-b">
+                      <td className="p-5 text-center">
+                        {user._id.substring(20, 24)}
+                      </td>
+                      <td className="p-5 text-center">{user.name}</td>
+                      <td className="p-5 text-center">{user.email}</td>
+                      <td className="p-5 text-center">
+                        {user.isAdmin ? "YES" : "NO"}
+                      </td>
+                      <td className="p-5 text-cyan-500 text-center">
+                        <button
+                          type="button"
+                          onClick={() => deleteHandler(user._id)}
+                        >
+                          Delete
+                        </button>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+          )}
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+AdminUsersScreen.auth = { adminOnly: true };
+export default AdminUsersScreen;
---
+++ meraki/pages/api/admin/users/[id].js
@@ -0,0 +1,35 @@
+/* eslint-disable no-else-return */
+import { getSession } from "next-auth/react";
+import { User } from "../../../../models";
+import { db } from "../../../../utils";
+
+const deleteHandler = async (req, res) => {
+  await db.connect();
+  const user = await User.findById(req.query.id);
+  if (user) {
+    if (user.email === "john@gmail.com") {
+      return res.status(400).send({ message: "Can not delete admin" });
+    }
+    await user.remove();
+    await db.disconnect();
+    res.send({ message: "User Deleted" });
+  } else {
+    await db.disconnect();
+    res.status(404).send({ message: "User Not Found" });
+  }
+};
+
+const handler = async (req, res) => {
+  const session = await getSession({ req });
+  if (!session || !session.user.isAdmin) {
+    return res.status(401).send("admin signin required");
+  }
+
+  if (req.method === "DELETE") {
+    return deleteHandler(req, res);
+  } else {
+    return res.status(400).send({ message: "Method not allowed" });
+  }
+};
+
+export default handler;
